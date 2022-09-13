67fd80ff4fb5b5e61f7c83f220e374360bc8b1c3
NexusUA <pbogomolny93@gmal.com>
2022-09-13 13:02:03 +0200

    - set up folders for views, models and view models - rename default content view to launch view

--- CitySightsApp/CitySightsApp.swift
+++ CitySightsApp/CitySightsApp.swift
@@ -1,5 +1,5 @@
 //
-//  CitySightsAppApp.swift
+//  CitySightsApp.swift
 //  CitySightsApp
 //
 //  Created by Pavel Bohomolnyi on 13/09/2022.
@@ -8,10 +8,10 @@
 import SwiftUI
 
 @main
-struct CitySightsAppApp: App {
+struct CitySightsApp: App {
     var body: some Scene {
         WindowGroup {
-            ContentView()
+            LaunchView()
         }
     }
 }
