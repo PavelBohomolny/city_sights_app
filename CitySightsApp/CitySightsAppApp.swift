68a3960b0dca8cbbeae1b8beef960a11dce06361
NexusUA <pbogomolny93@gmal.com>
2022-09-13 12:43:18 +0200

    Initial Commit

---
+++ CitySightsApp/CitySightsAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  CitySightsAppApp.swift
+//  CitySightsApp
+//
+//  Created by Pavel Bohomolnyi on 13/09/2022.
+//
+
+import SwiftUI
+
+@main
+struct CitySightsAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
